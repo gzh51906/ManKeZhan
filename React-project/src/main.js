8d459c5207bd700cfb35cbebdfa3bf24463e551a
YuanFang97 <yuanfang0097@gmail.com>
2019-09-20 14:22:29 +0800

    配置rem,scss

--- React-project/src/main.js
+++ React-project/src/main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from "react-dom";
 import {HashRouter} from "react-router-dom"
 import App from "./App";
+import './static/rem.js'
 render(
     <HashRouter><App /></HashRouter>
     ,
